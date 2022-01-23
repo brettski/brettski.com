2f3ff9ebb9d73260097d6fba26615f7dd7eb1207
Brett Slaski <brett@thatconference.com>
2022-01-23 09:11:42 -0600

    futzing around

--- svelte2/src/routes/__layout.svelte
+++ svelte2/src/routes/__layout.svelte
@@ -5,12 +5,13 @@
 <script>
   //import Header from "$component/header.svelte";
   import Header from "../lib/component/header.svelte";
+  import Footer from "../lib/component/footer.svelte";
 </script>
 
 <div>
   <Header />
-  <main>
+  <main class="h-3/4">
     <slot />
   </main>
-
+  <Footer />
 </div>
