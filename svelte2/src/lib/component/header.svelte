89fae0bc7d8869ef68400bd4ad6809f905f230a7
Brett Slaski <brett@thatconference.com>
2022-01-23 01:33:27 -0600

    rehashing this

---
+++ svelte2/src/lib/component/header.svelte
@@ -0,0 +1,50 @@
+<header class="bg-green-600">
+  <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+    <div class="w-full py-6 flex items-center justify-between border-b border-yellow-500 lg:border-none">
+      <div class="flex items-center">
+        <a href="/">
+          <span class="sr-only">Workflow</span>
+          <img class="h-10 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=black" alt="">
+        </a>
+        <div class="hidden ml-10 space-x-8 lg:block">
+          <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+            Blog
+          </a>
+
+          <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+            Tools
+          </a>
+
+          <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+            About
+          </a>
+
+          <!-- <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+            Company
+          </a> -->
+        </div>
+      </div>
+      <div class="ml-10 space-x-4">
+        <a href="/" class="inline-block bg-amber-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-amber-600">Sign in</a>
+        <a href="/" class="inline-block bg-slate-400 py-2 px-4 border border-transparent rounded-md text-base font-medium text-slate-50 hover:bg-slate-600">Sign up</a>
+      </div>
+    </div>
+    <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+      <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+        Blog
+      </a>
+
+      <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+        Tools
+      </a>
+
+      <a href="/" class="text-base font-medium text-white hover:text-indigo-50">
+        About
+      </a>
+
+      <!-- <a href="#" class="text-base font-medium text-white hover:text-indigo-50">
+        Company
+      </a> -->
+    </div>
+  </nav>
+</header>
---
+++ svelte2/src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  import '../tailwind.css';
+</script>
+
+<script>
+  //import Header from "$component/header.svelte";
+  import Header from "../lib/component/header.svelte";
+</script>
+
+<div>
+  <Header />
+  <main>
+    <slot />
+  </main>
+
+</div>
