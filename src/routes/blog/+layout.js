a5dcc43c79c1619e001b3c0cf7635851f488bbff
Brett Slaski <brettski@yahoo.com>
2023-06-28 22:54:41 -0500

    have a skeleton for blog page

--- src/routes/blog/+layout.js
+++ src/routes/blog/+layout.js
@@ -1,8 +1,9 @@
+import dayjs from 'dayjs';
 import { error } from '@sveltejs/kit';
 import { getPosts } from '$blog/getPosts';
 
 export async function load() {
-    let posts
+    let posts;
     try {
         posts = await getPosts();
         console.log('post count', posts?.length);
@@ -11,8 +12,26 @@ export async function load() {
         console.error(`error retrieving posts`, err);
         throw error(404, 'no posts');
     }
+    console.log('posts 0', posts[0])
+    const filterYear = new Set();
+    const filterCategory = new Set();
+    const filterTag = new Set();
+    posts = posts.map(p => {
+        p.categories.forEach(c => filterCategory.add(c));
+        p.tags.forEach(c => filterTag.add(c));
+        filterYear.add(dayjs(p.postDate).format('YYYY').toString())
+        const post = {
+        ...p,
+        postDate: dayjs(p.postDate).format('YYYY-MM-DD_HH:mm').toString(),
+        };
+
+        return post;
+    })
     
     return {
         posts,
+        filterYear: [...filterYear],
+        filterCategory: [...filterCategory],
+        filterTag: [...filterTag],
     }
 }
